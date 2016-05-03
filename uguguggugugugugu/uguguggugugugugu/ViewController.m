9faf5c66acaadfae16f7a69e20fdfcd9999d31ec
lk <746401552@qq.com>
2016-05-03 19:52:14 +0800

    fdgd

---
+++ uguguggugugugugu/uguguggugugugugu/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  uguguggugugugugu
+//
+//  Created by lk on 16/5/3.
+//  Copyright © 2016年 lk. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"s");
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
