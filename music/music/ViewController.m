512cb286c2a742dce4c01ed0b49b21a6673aab90
lkjy <2748517089@qq.com>
2016-05-03 20:31:42 +0800

    oo

---
+++ music/music/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  music
+//
+//  Created by lkjy on 16/5/3.
+//  Copyright © 2016年 M. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"dsw");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
